f11b7361d2f56b8bdd9a5c772093ce772f59ce3c
Erran Carey <me@errancarey.com>
2013-07-26 14:10:54 -0500

    Update wavsep::server

--- recipes/server.rb
+++ recipes/server.rb
@@ -12,9 +12,11 @@ include_recipe "apt"
 package "tomcat7"
 package "unzip"
 
+# TODO: Check the not_if statements...
+
 remote_file "#{Chef::Config[:file_cache_path]}/#{node[:wavsep][:zip_name]}" do
   action :create_if_missing
-  not_if "stat #{node[:wavsep][:dir]}"
+  not_if "stat #{node[:wavsep][:dir]}/wavsep"
   source node[:wavsep][:url]
 end
 
@@ -24,5 +26,5 @@ bash "install_wavsep" do
     unzip -q #{Chef::Config[:file_cache_path]}/#{node[:wavsep][:zip_name]} -d #{node[:wavsep][:dir]}
     rm #{Chef::Config[:file_cache_path]}/#{node[:wavsep][:zip_name]}
   eos
-  not_if "stat #{node[:wavsep][:dir]}"
+  not_if "stat #{node[:wavsep][:dir]}/wavsep"
 end
